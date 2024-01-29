f1c1deb03c11c183f70f16e24798b79649b4dce7
Eloy Castaño <eloycastano@gmail.com>
2024-01-29 12:47:19 +0100

    Autenticación i cerrar sesión

--- app/src/main/java/com/institutvidreres/winhabit/ui/login/AuthActivity.kt
+++ app/src/main/java/com/institutvidreres/winhabit/ui/login/AuthActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.institutvidreres.winhabit.MainActivity
+
 import com.institutvidreres.winhabit.databinding.ActivityAuthBinding
 
 class AuthActivity : AppCompatActivity() {
